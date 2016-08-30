fa5b7573f26a8ea9e2ffd5be666e1ad0d8843f6d
Hiren <hiren7884@gmail.com>
2016-08-30 17:29:18 +0530

    commit ini..

--- app/src/main/java/com/example/hirenamaliyar/attendenceemployee/Person_details.java
+++ app/src/main/java/com/example/hirenamaliyar/attendenceemployee/Person_details.java
@@ -1,13 +1,13 @@
 package com.example.hirenamaliyar.attendenceemployee;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 public class Person_details extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +15,5 @@ public class Person_details extends AppCompatActivity {
         setContentView(R.layout.activity_person_details);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
     }
 }
