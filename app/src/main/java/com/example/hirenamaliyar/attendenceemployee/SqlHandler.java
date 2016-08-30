3a23b4f90f4775415a7447df305e8e425cae41af
Hiren <hiren7884@gmail.com>
2016-08-30 11:38:00 +0530

    first commit

---
+++ app/src/main/java/com/example/hirenamaliyar/attendenceemployee/SqlHandler.java
@@ -0,0 +1,61 @@
+package com.example.hirenamaliyar.attendenceemployee;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+public class SqlHandler {
+
+    public static final String DATABASE_NAME = "Employee_Attendence";
+    public static final int DATABASE_VERSION = 1;
+    Context context;
+    SQLiteDatabase sqlDatabase;
+    SqlDbHelper dbHelper;
+
+    public SqlHandler(Context context) {
+
+        dbHelper = new SqlDbHelper(context, DATABASE_NAME, null,
+                DATABASE_VERSION);
+        sqlDatabase = dbHelper.getWritableDatabase();
+    }
+
+    //for exexecute query
+    public void executeQuery(String query) {
+        try {
+
+            if (sqlDatabase.isOpen()) {
+                sqlDatabase.close();
+            }
+
+            sqlDatabase = dbHelper.getWritableDatabase();
+            sqlDatabase.execSQL(query);
+
+        } catch (Exception e) {
+
+            System.out.println("DATABASE ERROR " + e);
+        }
+
+    }
+
+    //for select query
+    public Cursor selectQuery(String query) {
+        Cursor c1 = null;
+        try {
+
+            if (sqlDatabase.isOpen()) {
+                sqlDatabase.close();
+
+            }
+            sqlDatabase = dbHelper.getWritableDatabase();
+            c1 = sqlDatabase.rawQuery(query, null);
+
+        } catch (Exception e) {
+
+            System.out.println("DATABASE ERROR " + e);
+
+        }
+        return c1;
+
+    }
+
+}
